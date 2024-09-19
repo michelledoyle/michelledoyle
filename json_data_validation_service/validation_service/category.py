4b6e09aaecac688767e08c2898893db7b74d52e4
michelle <michelle.doyle@ascension.org>
2024-09-19 18:41:22 -0500

    update

--- json_data_validation_service/validation_service/category.py
+++ json_data_validation_service/validation_service/category.py
@@ -1,7 +1,12 @@
 from enum import Enum
 
+
 class Category(Enum):
     PATIENT = "Patient"
     ENCOUNTER = "Encounter"
     PATIENT_ADDRESS = "PatientAddress"
     PATIENT_PHONE = "PatientPhone"
+
+    @property
+    def value_str(self):
+        return self.value
